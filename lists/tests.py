1f175d2d1e4ffc9de63689500bf3aec5abc72c93
raultr <RAUL.TORRES@DIR-TEC-RAUL.cusaem.gob.mx>
2015-09-04 15:17:16 -0500

    Primer test unitario probando una url, con una vista tonta

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,8 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-class SmokeTest(TestCase):
-
-	def test_bad_math(self):
-		self.assertEqueal(1 + 1, 3 )
+class HomePageTest(TestCase):
+	def test_root_url_encia_al_home(self):
+		found = resolve('/')
+		self.assertEqual(found.func,home_page)
