6f0b6110f47c50ee5a6bab330815ddb1c84faf96
raultr <RAUL.TORRES@DIR-TEC-RAUL.cusaem.gob.mx>
2015-09-07 17:57:40 -0500

    Test funcional para revisar si podemos ingresar una tarea

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 #Como es un caso de pruebas lo heredamos de unittest.TestCase
@@ -19,22 +20,45 @@ class NewVisitorTest(unittest.TestCase):
 
 		#Ella revisa que el titulo y cabecero
 		self.assertIn('To-Do',self.browser.title) #Documentacion de los asserts https://docs.python.org/3/library/unittest.html
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
+		#la caja de texto debe tener una invitacion a ingresas una tarea
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+				inputbox.get_attribute('placeholder'),
+				'Enter a to-do item'
+		)
+
+		#Digitamos en la caja de texto
+		inputbox.send_keys('Buy peacock feathers')
+
+		#Tecleamos enter para se agregue la tarea
+		inputbox.send_keys('keys.ENTER')
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_element_by_tag_name('tr')
+		self.assertTrue(
+				any(row.text == '1:Buy peacock feathers' for row in rows)
+		)
+
+
 		self.fail('Finish the test!') #Se usa para informar que el test termino
 	
 if __name__ == '__main__': #Si este script se ejecuta desde la linea de comandos y no se esta importando
 	unittest.main(warnings='ignore') #Ejecutamos el programa principal de unittest el cual encontrara las clases y metodos
 #browser = webdriver.Firefox()
-# Revisar la direccion principal
+#Ok Revisar la direccion principal
 
-# El titulo y cabecero de la pagina debe de contener el titulo
+#Ok El titulo y cabecero de la pagina debe de contener el titulo
 #assert 'To-Do' in browser.title
 
-# She is invited to enter a to-do item straight away
+#Ok She is invited to enter a to-do item straight away
 
-# She types "Buy peacock feathers" into a text box (Edith's hobby
+#Ok She types "Buy peacock feathers" into a text box (Edith's hobby
 # is tying fly-fishing lures)
 
-# When she hits enter, the page updates, and now the page lists
+#Ok When she hits enter, the page updates, and now the page lists
 # "1: Buy peacock feathers" as an item in a to-do list
 
 # There is still a text box inviting her to add another item. She
