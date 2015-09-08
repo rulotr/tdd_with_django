3bcfff04c437e96393aa6e647b2ffb3ff863912b
raultr <RAUL.TORRES@DIR-TEC-RAUL.cusaem.gob.mx>
2015-09-08 15:24:49 -0500

    Primera refactoriazacion, se uso una plantilla en lugar de un text plano

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -11,9 +12,9 @@ class HomePageTest(TestCase):
 	def test_pagina_principal_retorna_un_html_correcto(self):
 		request  = HttpRequest()
 		response = home_page(request)
-		self.assertTrue(response.content.startswith(b'<html>'))
-		self.assertIn(b'<title>To-Do lists</title>', response.content)
-		self.assertTrue(response.content.endswith(b'</html>'))
+		expected_html = render_to_string('home.html')
+		self.assertEqual(response.content.decode(),expected_html) #decode convierte bytes a cadenas unicodes
 
+		
 
 #Probar los test python manage.py test
--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-	return HttpResponse('<html><title>To-Do lists</title></html>')
+	return render(request,'home.html') # En lugar de usar HttpResponse, se usa render, necesita un request y la plantilla
