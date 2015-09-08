dd649866996108706ecede12921ec0e4672822c5
raultr <RAUL.TORRES@DIR-TEC-RAUL.cusaem.gob.mx>
2015-09-08 17:20:38 -0500

    Se agrego el modelo para lists

--- lists/tests.py
+++ lists/tests.py
@@ -3,8 +3,10 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.views import home_page
+from lists.models import Item
 
 class HomePageTest(TestCase):
+	
 	def test_root_url_encia_al_home(self):
 		found = resolve('/')
 		self.assertEqual(found.func,home_page)
@@ -15,6 +17,39 @@ class HomePageTest(TestCase):
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(),expected_html) #decode convierte bytes a cadenas unicodes
 
-		
+	def test_pagina_principal_puede_guardar_un_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+
+		response = home_page(request)
+
+		self.assertIn('A new list item', response.content.decode())
+		expected_html = render_to_string(
+				'home.html',
+				{'new_item_text': 'A new list item'}
+			)
+		self.assertEqual(response.content.decode(), expected_html)
+
+class ItemModelTest(TestCase):
+	
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text ="The first (ever) list item"
+		first_item.save()
+
+		second_item = Item()
+		second_item.text ="Item the second"
+		second_item.save()
+
+		save_items = Item.objects.all()
+		self.assertEqual(save_items.count(), 2)
+
+		first_saved_item = save_items[0]
+		second_saved_item = save_items[1]
+		self.assertEqual(first_saved_item.text, "The first (ever) list item")
+		self.assertEqual(second_saved_item.text, "Item the second")
+
+
 
 #Probar los test python manage.py test
--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-	return render(request,'home.html') # En lugar de usar HttpResponse, se usa render, necesita un request y la plantilla
+	return render(request,'home.html',{ #item_Text es el name del input con el que se asocia
+		'new_item_text': request.POST.get('item_text',''),
+	}) # En lugar de usar HttpResponse, se usa render, necesita un request y la plantilla
